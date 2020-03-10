f3f309861c292e8456b5593a929cf88c6dc056c8
DRKSLV <56208328+DRKSLV@users.noreply.github.com>
2020-03-10 10:35:33 +0100

    Add file loaders (must be imported later)

--- next.config.js
+++ next.config.js
@@ -18,7 +18,19 @@ module.exports = withSass(withCSS({
       new webpack.DefinePlugin({
         'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
       }),
-    )
+    );
+    config.rules.push({
+       test: /\.(png|svg|jpg|gif)$/,
+       use: [
+          'file-loader',
+       ]
+    });
+    config.rules.push({
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: [
+            'file-loader',
+          ]
+    });
 
     return config
   }
