b9453573a1f7727c805706c0867d4176ca7109cf
DRKSLV <56208328+DRKSLV@users.noreply.github.com>
2020-03-13 16:06:43 +0100

    Mobbing IE Users

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,30 @@
 
 import Header from "../components/layout/header"
 import "../style/style.sass";
+import { useEffect, useState } from "react";
 
 const App = (props) => {
+    var [browserwarning, setWarning] = useState(null);
+
+    useEffect(() => {
+        if(msieversion() !== 0) {
+            setWarning(<p style={{color:"red", textAlign:"center"}}><b>No Internet Explorer Support go away</b></p>);
+        }
+    }, []);
 
     return (
         <>
             <div className="container">
                 <noscript>JavaScript: Some features are diabled!</noscript>
-                <p style={{textAlign:"center"}}>
-                    HELLO CITIZENS
-                    <br/>
-                    <b>WELCOME</b>
-                </p>
+                { 
+                    browserwarning === null ?
+                        <p style={{textAlign:"center"}}>
+                            HELLO CITIZENS
+                            <br/>
+                            <b>WELCOME</b>
+                        </p>
+                    : browserwarning
+                }
             </div>
         </>
     );
@@ -22,3 +34,20 @@ const Index = () => (<App />);
 Index.getLayout = page => <><Header />{page}</>;
 
 export default Index;
+
+
+
+function msieversion() {
+
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
+    {
+        return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)));
+    }
+    else  // If another browser, return 0
+    {
+        return 0;
+    }
+}
